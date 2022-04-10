1bc54da344fc2df8fd43dd6fca1ed466b0d89acf
Nhat Anh <NhatAnh@Admins-MacBook-Pro-2.local>
2022-04-10 13:01:04 +0700

    feat: add table matrix

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,31 +1,114 @@
-import React, {useState} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 
 export const CustomComponent: React.FC = () => {
-  const [val, setVal] = useState<String>('');
+  const val = useRef<String>('');
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+    val: 0,
+  });
   const [isShow, setIsShow] = useState<Boolean>(false);
-  const [arrTable, setArrTable] = useState<String[]>([]);
+  const [arrTable, setArrTable] = useState<number[][]>([]);
 
-  const onChangeInput = e => {
-    setVal(e.target.value);
+  const onChangeInput = useCallback((e: any) => {
+    try {
+      if (!e.target.value) return;
+      val.current = e.target.value;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
+
+  const onClickExe = useCallback(() => {
+    try {
+      if (!val.current) return;
+      const arr = [...Array(Number(val.current))].map((_, i: number) => {
+        return [...Array(Number(val.current))].map((_, j: number) => {
+          if (j % 2 === 0) {
+            return j * Number(val.current) + i + 1;
+          } else {
+            return (j + 1) * Number(val.current) - i;
+          }
+        }, []);
+      }, []);
+
+      setArrTable(arr);
+      setIsShow(true);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
+
+  const handleOnDrag = React.useCallback((e: any) => {
+    try {
+      const id: string = e.target.id;
+      const arr = id.split('_');
+      setPosition({
+        x: Number(arr[0]),
+        y: Number(arr[1]),
+        val: Number(e.target.innerHTML),
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
+
+  const handleOnDrop = React.useCallback(
+    (e: any) => {
+      try {
+        const id: string = e.target.id;
+        const arr = id.split('_');
+        const value = e.target.innerHTML;
+        const tempArr = [...arrTable];
+
+        tempArr[position.x][position.y] = Number(value);
+        tempArr[arr[0]][arr[1]] = position.val;
+        setArrTable(tempArr);
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    [arrTable, position],
+  );
+
+  const allowDrop = (e: any) => {
+    try {
+      e.preventDefault();
+    } catch (error) {
+      throw new Error(error);
+    }
   };
 
-  const onClickExe = () => {
-    const arr = [...Array(Number(val) * Number(val))].map(
-      (_, index: number) => {
-        return index.toString();
-      },
-    );
-
-    setArrTable(arr);
-    setIsShow(true);
+  const onEnterEvent = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    try {
+      if (event.key === 'Enter') {
+        onClickExe();
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
   };
 
-  console.log('arrTable', arrTable);
-  // console.log('val', val);
+  const onKeyDownEvent = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    try {
+      event.return;
+      if (event.key === 'Enter') {
+        onClickExe();
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
   return (
     <>
-      <input placeholder={'Please input number'} onChange={onChangeInput} />
+      <input
+        placeholder={'Please input number'}
+        onChange={onChangeInput}
+        onKeyUp={onEnterEvent}
+        onKeyDown={onKeyDownEvent}
+        type="number"
+      />
       <br />
       <button onClick={onClickExe}>Exe</button>
       <br />
@@ -34,13 +117,25 @@ export const CustomComponent: React.FC = () => {
       {isShow && (
         <table>
           <tbody>
-            {[...Array(Number(val))].map((_, index: number) => {
+            {[...Array(Number(val.current))].map((_, i: number) => {
               return (
-                <tr key={`first${index}`}>
-                  {[...Array(Number(val))].map((_, indexSecond: number) => {
+                <tr key={`first${i}`}>
+                  {[...Array(Number(val.current))].map((_, j: number) => {
                     return (
-                      <td key={`second${indexSecond}`}>
-                        {arrTable[Number(indexSecond)]}
+                      <td
+                        style={{
+                          border: '1px solid',
+                          padding: '10px',
+                          textAlign: 'center',
+                        }}
+                        key={`second${j}`}
+                        id={`${i}_${j}`}
+                        draggable={true}
+                        onDragStart={handleOnDrag}
+                        onDrop={handleOnDrop}
+                        onDragOver={allowDrop}
+                      >
+                        {arrTable[Number(i)][Number(j)]}
                       </td>
                     );
                   })}
@@ -50,26 +145,6 @@ export const CustomComponent: React.FC = () => {
           </tbody>
         </table>
       )}
-
-      <table>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>5</td>
-            <td>8</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>4</td>
-            <td>9</td>
-          </tr>
-        </tbody>
-      </table>
     </>
   );
 };
