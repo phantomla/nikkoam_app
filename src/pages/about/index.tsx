d346ec104bf55cc779e4b831f1f0255f828ddb13
Nhat Anh <NhatAnh@Admins-MacBook-Pro-2.local>
2022-04-10 14:45:20 +0700

    chore: test drag

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -41,6 +41,8 @@ export const CustomComponent: React.FC = () => {
 
   const handleOnDrag = React.useCallback((e: any) => {
     try {
+      console.log('event Start', e.target.innerHTML);
+
       const id: string = e.target.id;
       const arr = id.split('_');
       setPosition({
@@ -56,6 +58,8 @@ export const CustomComponent: React.FC = () => {
   const handleOnDrop = React.useCallback(
     (e: any) => {
       try {
+        console.log('event End', e.target.innerHTML);
+
         const id: string = e.target.id;
         const arr = id.split('_');
         const value = e.target.innerHTML;
@@ -99,6 +103,61 @@ export const CustomComponent: React.FC = () => {
     }
   };
 
+  const onTouchStart = (e: any) => {
+    try {
+      const id: string = e.target.id;
+      const arr = id.split('_');
+      setPosition({
+        x: Number(arr[0]),
+        y: Number(arr[1]),
+        val: Number(e.target.innerHTML),
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  console.log('positin', position);
+
+  const onTouchEnd = (event: any) => {
+    try {
+      const currentId_X = Math.round(
+        (Number(event.target.style.top.replace('px', '')) - 50) / 40,
+      );
+
+      const currentId_Y = Math.round(
+        (Number(event.target.style.left.replace('px', '')) - 50) / 40,
+      );
+
+      const box = document.getElementById(`${currentId_X}_${currentId_Y}`);
+
+      event.target.innerHTML = box.innerHTML;
+      box.innerHTML = position.val.toString();
+
+      box.style.top = currentId_X * 40 + 50 + 'px';
+      box.style.left = currentId_Y * 40 + 50 + 'px';
+
+      event.target.style.top = position.x * 40 + 50 + 'px';
+      event.target.style.left = position.y * 40 + 50 + 'px';
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  const onTouchMove = (event: any) => {
+    try {
+      const touchLocation = event.targetTouches[0];
+
+      const box = document.getElementById(`${position.x}_${position.y}`);
+      // const box = document.getElementById(`test`);
+
+      // assign box new coordinates based on the touch.
+      box.style.left = touchLocation.pageX + 'px';
+      box.style.top = touchLocation.pageY + 'px';
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
   return (
     <>
       <input
@@ -113,42 +172,46 @@ export const CustomComponent: React.FC = () => {
       <br />
       <br />
       <br />
-      {
-        isShow &&
-          // <table>
-          //   <tbody>
-          [...Array(Number(val.current))].map((_, i: number) => {
-            return (
-              <tr key={`first${i}`}>
-                {[...Array(Number(val.current))].map((_, j: number) => {
-                  return (
-                    <td
-                      style={{
-                        border: '1px solid',
-                        padding: '10px',
-                        textAlign: 'center',
-                      }}
-                      key={`second${j}`}
-                      id={`${i}_${j}`}
-                      draggable={true}
-                      onDragStart={handleOnDrag}
-                      onDrop={handleOnDrop}
-                      onDragOver={allowDrop}
-                      onTouchStart={handleOnDrag}
-                      onTouchEnd={handleOnDrop}
-                    >
-                      {arrTable[Number(i)][Number(j)]}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })
-        //   </tbody>
-        // </table>
-      }
-      <td draggable={true}>12312323</td>
-      <div draggable={true}>12312323</div>
+      {isShow && (
+        <table>
+          <tbody>
+            {[...Array(Number(val.current))].map((_, i: number) => {
+              return (
+                <tr key={`first${i}`}>
+                  {[...Array(Number(val.current))].map((_, j: number) => {
+                    return (
+                      <td
+                        style={{
+                          border: '1px solid',
+                          padding: '10px',
+                          textAlign: 'center',
+                          cursor: 'pointer',
+                          position: 'absolute',
+                          top: i * 40 + 50 + 'px',
+                          left: j * 40 + 50 + 'px',
+                          height: '40px',
+                          width: '40px',
+                        }}
+                        key={`second${j}`}
+                        id={`${i}_${j}`}
+                        draggable={true}
+                        onDragStart={handleOnDrag}
+                        onDrop={handleOnDrop}
+                        onDragOver={allowDrop}
+                        onTouchStart={onTouchStart}
+                        onTouchMove={onTouchMove}
+                        onTouchEnd={onTouchEnd}
+                      >
+                        {arrTable[Number(i)][Number(j)]}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
