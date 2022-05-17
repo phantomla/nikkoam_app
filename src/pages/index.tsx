990835a010a5fe8de029ea12a041aaacabd50628
PhantomNA <nguyennhatanh96@gmail.com>
2022-05-17 10:39:40 +0700

    test csp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,10 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self'; base-uri 'self';"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;1,100;1,300&display=swap"
           rel="preload"
